b16d042bfac774d0333deb29b1c1becaf1820e13
Suman Dangi <sumankdangi26@gmail.com>
2024-01-05 10:43:31 +0545

    Message

---
+++ Validation/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+# Create your views here.
+def home(request):
+    return render(request,"Validation/index.html")
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname= request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, "Your Account Successfully Created.")
+
+        return redirect('signin')
+
+    return render(request, "Validation/signup.html")
+
+def signin(request):
+    if request.method =='POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username,pass1)
+
+        if user is not None:
+            login(request,user)
+            fname = user.first_name
+            return render(request,"Validation/index.html",{'fname':fname})
+
+        else:
+            messages.error(request, "Bad Credentials!")
+            return redirect('home')
+
+
+
+    return render(request, "Validation/signin.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request,"Logged Out Successfully")
+    return redirect('home')
+
